b359ae90cf2b4ac49853e035449d74d3fed39cae
BenD <61838571+BenDDavidson@users.noreply.github.com>
2023-05-07 07:48:54 +0100

    revert popup.js

--- Homepage/popup.js
+++ Homepage/popup.js
@@ -9,20 +9,20 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
-  modal.style.display = "block";
+    modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+    modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-} 
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 // Get the date input element
 const dateInput = document.getElementById("dateSelecter");
@@ -34,74 +34,46 @@ const selectedDate = dateInput.value;
 console.log("Selected date: " + selectedDate);
 
 function saveDate() {
-  // Get the date input element
-  const dateInput = document.getElementById("dateSelecter");
+    // Get the date input element
+    const dateInput = document.getElementById("dateSelecter");
 
-  // Get the value of the date input element and save it as a variable
-  const selectedDate = dateInput.value;
+    // Get the value of the date input element and save it as a variable
+    const selectedDate = dateInput.value;
 
-  return  Date.parse(selectedDate)
+    return  Date.parse(selectedDate)
 
 }
 
 function saveEndDate() {
-  // Get the date input element
-  const dateInput = document.getElementById("endDateSelecter");
+    // Get the date input element
+    const dateInput = document.getElementById("endDateSelecter");
 
-  // Get the value of the date input element and save it as a variable
-  const selectedEndDate = dateInput.value;
+    // Get the value of the date input element and save it as a variable
+    const selectedEndDate = dateInput.value;
 
-  return Date.parse(selectedEndDate)
+    return Date.parse(selectedEndDate)
 }
 
 function saveTitle() {
-  var title = document.getElementById("title").value;
-  return String(title)
+    var title = document.getElementById("title").value;
+    return String(title)
 }
 
 function saveTextarea() {
-  var textarea = document.getElementById("textarea").value;
-  return String(textarea)
+    var textarea = document.getElementById("textarea").value;
+    return String(textarea)
 }
 
 function saveChecklist() {
-  var option1 = document.getElementById("option1").checked;
-  var option2 = document.getElementById("option2").checked;
-  var option3 = document.getElementById("option3").checked;
-  var option4 = document.getElementById("option4").checked;
+    var option1 = document.getElementById("option1").checked;
+    var option2 = document.getElementById("option2").checked;
+    var option3 = document.getElementById("option3").checked;
+    var option4 = document.getElementById("option4").checked;
 
-  return [option1,option2,option3,option4]
+    return [option1,option2,option3,option4]
 }
 
-//THIS CREATES 2 NEW ITEMS IN ONE COLUMN using form
-//adapt to here
-function saveAll() {
-    // Get the values of the form inputs
-    const title = document.querySelector(".title").value;
-    const description = document.querySelector(".description").value;
-    const tags = [];
-    document.querySelectorAll('input[type=checkbox]:checked').forEach(tag => {
-        tags.push(tag.value);
-    });
-    const startDate = document.querySelector("#dateSelecter").value;
-    const endDate = document.querySelector("#endDateSelecter").value;
-
-    // Create a new event board item with the collected data
-    const eventBoardItem = document.createElement("div");
-    eventBoardItem.classList.add("eventBoard__Item-input");
-
-    const itemContent = document.createTextNode(`${title}: ${description}`);
-    eventBoardItem.appendChild(itemContent);
-
-    // Find the first event board and add the new item to it
-    allEventBoards.forEach(eventBoard => {
-        eventBoard.appendChild(eventBoardItem.cloneNode(true));
-    });
-
-    // Close the modal dialog
-    modal.style.display = "none";
+function saveAll(){
+    var eventValue = [saveTitle(), saveTextarea(),saveChecklist(),saveDate(),saveEndDate()]
+    console.log(eventValue)
 }
-
-
-const saveButton = document.querySelector(".modal-content button");
-saveButton.addEventListener("click", saveAll);
