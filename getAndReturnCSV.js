fbe066f8078cfa70f58255bd0ecb8da9c95868e2
Robert Spear <robert.s.spear@gmail.com>
2023-05-11 18:38:43 +0100

    fixed getcsv so that it is asynchronous and added getEvent function to allow for retrieval of single element

--- getAndReturnCSV.js
+++ getAndReturnCSV.js
@@ -13,64 +13,69 @@ class Event {
 
 	set changeTitle(newTitle) {this.title = newTitle;}
 	// get title(index){return events(index).this.title;}
-	
 	set changeDescription(newDescription) {this.description = newDescription;}
 	// get description(){return this.description;}
-	
 	set changeTagList(newTaglist) {this.tagList= newTaglist;}
 	// get tagList(){return this.tagList;}
-	
-
 	set changeStartDate(newStartDate) {this.startDate = newStartDate;}
 	// get startDate(){return this.startDate;}
-	
+
 	set changeEndDate(newEndDate) {this.endDate= this.newEndDate;}
 	// get endDate(){return this.endDate;}
 
 }
 
-const events = [];
-function fromCSV (){
-fs.createReadStream('events.csv')
-	.pipe(parse())
-	.on('data',(row)=>{
-		const title = row[0];
-		const description = row[1];
-		const tagList = [
-			row[2] === 'true',
-			row[3] === 'true',
-			row[4] === 'true',
-			row[5] === 'true'
-		];
-		const startDate = new Date(parseInt(row[6]));
-		const endDate = new Date(parseInt(row[7]));
-		const event = new Event(title,description,tagList,startDate,endDate);
-
-		events.push(event);
-	})
-	.on ('end',()=>{
-		console.log(events);
-	})
+let events = [];
+async function fromCSV() {
+	const csvData = await fs.promises.readFile('events.csv', 'utf-8');
+  
+	return new Promise((resolve, reject) => {
+	  parse(csvData, { delimiter: ',' })
+		.on('readable', function () {
+		  let row;
+		  while ((row = this.read())) {
+			const title = row[0];
+			const description = row[1];
+			const tagList = [
+			  row[2] === 'true',
+			  row[3] === 'true',
+			  row[4] === 'true',
+			  row[5] === 'true'
+			];
+			const startDate = new Date(parseInt(row[6]));
+			const endDate = new Date(parseInt(row[7]));
+			const event = new Event(title, description, tagList, startDate, endDate);
+  
+			events.push(event);
+		  }
+		})
+		.on('end', function () {
+		  resolve(events);
+		})
+		.on('error', function (error) {
+		  reject(error);
+		});
+	});
 }
 
-function addEvent(addTitle,addDescription,addTag1,addTag2,addTag3,addTag4,addStartDate,addEndDate){
-	const title = addTitle;
-	const description = addDescription;
-	const tagList = [
-		addTag1,addTag2,addTag3,addTag4
-	];
-	const startDate = addStartDate;
-	const endDate = addEndDate;
-	
-	const addEvents = new Event(title,description,tagList,startDate,endDate);
-	const csv = (title+','+description+','+tagList+','+startDate+','+endDate);
-	fs.appendFile('events.csv', csv, (err) => {
-		if (err) console.error('Couldn\'t append the data');})
-	
+async function getArray(){
+	const event = await fromCSV();
+	console.log(events);
+	return event;
 }
 
+async function getEvent(input){
+	const event = await fromCSV();
+	console.log(events[input]);
+	return event;
+}
 
-fromCSV(); 
+console.log(getArray());
+var input = 1;
+console.log(getEvent(input));
+
+
+
 
 
 // addTitle="food 3";
@@ -92,6 +97,21 @@ fromCSV();
 
 
 
+function addEvent(addTitle,addDescription,addTag1,addTag2,addTag3,addTag4,addStartDate,addEndDate){
+	const title = addTitle;
+	const description = addDescription;
+	const tagList = [
+		addTag1,addTag2,addTag3,addTag4
+	];
+	const startDate = addStartDate;
+	const endDate = addEndDate;
+	
+	const addEvents = new Event(title,description,tagList,startDate,endDate);
+	const csv = (title+','+description+','+tagList+','+startDate+','+endDate);
+	fs.appendFile('events.csv', csv, (err) => {
+		if (err) console.error('Couldn\'t append the data');})
+	
+}
 
 
 
