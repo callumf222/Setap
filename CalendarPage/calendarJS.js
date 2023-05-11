6704e5f92c2e6a215a0d1177f811ce9c0cc1f865
Callumf222 <callumfry57@gmail.com>
2023-05-11 16:12:43 +0100

    Calendar Branch

--- CalendarPage/calendarJS.js
+++ CalendarPage/calendarJS.js
@@ -1,64 +1,73 @@
-const daysTag = document.querySelector(".days"),
-    currentDate = document.querySelector(".currentDate"),
-    prevNextIcon = document.querySelectorAll(".icons span");
+    const daysTag = document.querySelector(".days"),
+        currentDate = document.querySelector(".currentDate"),
+        prevNextIcon = document.querySelectorAll(".icons span");
 
 
 
-let date = new Date(),
-    currYear = date.getFullYear(),
-    currMonth = date.getMonth();
+    let date = new Date(),
+        currYear = date.getFullYear(),
+        currMonth = date.getMonth();
 
-const months = ["January", "February", "March", "April", "May", "June", "July",
-    "August", "September", "October", "November", "December"];
+    const months = ["January", "February", "March", "April", "May", "June", "July",
+        "August", "September", "October", "November", "December"];
 
-const renderCalendar = () => {
-    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(),
-        lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(),
-        lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(),
-        lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
-    let liTag = "";
+    const renderCalendar = () => {
+        let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(),
+            lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(),
+            lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(),
+            lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
+        let liTag = "";
 
-    for (let i = firstDayofMonth; i > 0; i--) {
-        liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
-    }
-
-    for (let i = 1; i <= lastDateofMonth; i++) {
-        let isToday = i === date.getDate() && currMonth === new Date().getMonth()
-        && currYear === new Date().getFullYear() ? "active" : "";
-        liTag += `<li class="${isToday}">${i}</li>`;
-    }
-
-    for (let i = lastDayofMonth; i < 6; i++) {
-        liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`
-    }
-
-    currentDate.innerText = `${months[currMonth]} ${currYear}`;
-    daysTag.innerHTML = liTag;
-}
-
-renderCalendar();
-prevNextIcon.forEach(icon => {
-    icon.addEventListener("click", () => {
-        currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
-        if(currMonth < 0 || currMonth > 11) {
-
-            date = new Date(currYear, currMonth, new Date().getDate());
-            currYear = date.getFullYear();
-            currMonth = date.getMonth();
-        } else {
-            date = new Date();
+        for (let i = firstDayofMonth; i > 0; i--) {
+            liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
         }
-        renderCalendar();
-    });
-});
 
+        for (let i = 1; i <= lastDateofMonth; i++) {
+            let isToday = i === date.getDate() && currMonth === new Date().getMonth()
+            && currYear === new Date().getFullYear() ? "active" : "";
+            liTag += `<li class="${isToday}">${i}</li>`;
+        }
 
-function pickDate() {
-    const monthInput = document.querySelector('input[type="month"]');
-    const [year, month] = monthInput.value.split('-').map(Number);
-    
-    currYear = year;
-    currMonth = month - 1;
+        for (let i = lastDayofMonth; i < 6; i++) {
+            liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`
+        }
+
+        currentDate.innerText = `${months[currMonth]} ${currYear}`;
+        daysTag.innerHTML = liTag;
+    }
 
     renderCalendar();
-  }
+    prevNextIcon.forEach(icon => {
+        icon.addEventListener("click", () => {
+            currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+            if(currMonth < 0 || currMonth > 11) {
+
+                date = new Date(currYear, currMonth, new Date().getDate());
+                currYear = date.getFullYear();
+                currMonth = date.getMonth();
+            } else {
+                date = new Date();
+            }
+            renderCalendar();
+        });
+    });
+
+
+    function pickDate() {
+        const monthInput = document.querySelector('input[type="month"]');
+        const [year, month] = monthInput.value.split('-').map(Number);
+        
+        currYear = year;
+        currMonth = month - 1;
+
+        renderCalendar();
+    }
+
+
+
+
+      
+      
+      
+    
+    
