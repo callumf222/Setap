72e4ac3c486db5d9d610715a2aeb2d333cbf05d3
Robert Spear <robert.s.spear@gmail.com>
2023-05-12 14:47:10 +0100

    Fixed getters and setter and added comments on how to use various functions

--- getAndReturnCSV.js
+++ getAndReturnCSV.js
@@ -11,21 +11,27 @@ class Event {
 		this.endDate = endDate;
 	}
 
+	//---------------Getters and setters for the Event objects----------------
 	set changeTitle(newTitle) {this.title = newTitle;}
-	// get title(index){return events(index).this.title;}
-	set changeDescription(newDescription) {this.description = newDescription;}
-	// get description(){return this.description;}
-	set changeTagList(newTaglist) {this.tagList= newTaglist;}
-	// get tagList(){return this.tagList;}
-	set changeStartDate(newStartDate) {this.startDate = newStartDate;}
-	// get startDate(){return this.startDate;}
+	get getTitle(){return this.title;}
 
-	set changeEndDate(newEndDate) {this.endDate= this.newEndDate;}
-	// get endDate(){return this.endDate;}
+	set changeDescription(newDescription) {this.description = newDescription;}
+	get getDescription(){return this.description;}
+
+	set changeTagList(newTaglist) {this.tagList= newTaglist;}
+	get getTagList(){return this.tagList;}
+
+	set changeStartDate(newStartDate) {this.startDate = newStartDate;}
+	get getStartDate(){return this.startDate;}
+
+	set changeEndDate(newEndDate) {this.endDate = newEndDate;}
+	get getEndDate(){return this.endDate;}
 
 }
 
-let events = [];
+
+
+// fromCSV this takes the csv used in app and asynchronously reads and converts it into the array we use for storage  
 async function fromCSV() {
 	const csvData = await fs.promises.readFile('events.csv', 'utf-8');
   
@@ -34,6 +40,8 @@ async function fromCSV() {
 		.on('readable', function () {
 		  let row;
 		  while ((row = this.read())) {
+
+			// code here responisible for taking the csv line and spliting it into the correct layout
 			const title = row[0];
 			const description = row[1];
 			const tagList = [
@@ -42,15 +50,16 @@ async function fromCSV() {
 			  row[4] === 'true',
 			  row[5] === 'true'
 			];
-			const startDate = new Date(parseInt(row[6]));
+			const startDate = new Date(parseInt(row[6])); // date is storred in csv as a string of numbers (Epoch time) then converted to javascript date datatype
 			const endDate = new Date(parseInt(row[7]));
-			const event = new Event(title, description, tagList, startDate, endDate);
-  
-			events.push(event);
+			// event is created  then pushed to the array
+			const event = new Event(title, description, tagList, startDate, endDate); 
+			eventsList.push(event);
+
 		  }
 		})
 		.on('end', function () {
-		  resolve(events);
+		  resolve(eventsList);
 		})
 		.on('error', function (error) {
 		  reject(error);
@@ -58,6 +67,8 @@ async function fromCSV() {
 	});
 }
 
+
+// adds event to the csv 
 function addEvent(addTitle,addDescription,addTag1,addTag2,addTag3,addTag4,addStartDate,addEndDate){
 	const title = addTitle;
 	const description = addDescription;
@@ -67,56 +78,46 @@ function addEvent(addTitle,addDescription,addTag1,addTag2,addTag3,addTag4,addSta
 	const startDate = addStartDate;
 	const endDate = addEndDate;
 	
-	const addEvents = new Event(title,description,tagList,startDate,endDate);
-	const csv = (title+','+description+','+tagList+','+startDate+','+endDate);
+	const event = new Event(title,description,tagList,startDate,endDate);
+	eventsList.push(event);
+	const csv = ("\n"+title+','+description+','+tagList+','+startDate+','+endDate);
 	fs.appendFile('events.csv', csv, (err) => {
 		if (err) console.error('Couldn\'t append the data');})
 	
 }
 
+// used to get the list created in from CSV 
 async function getArray(){
-	const event = await fromCSV();
-	console.log(events);
-	return event;
+	const eventsList = await fromCSV();
+	//console.log(events);
+	return eventsList;
 }
 
+// get event from specific index in the array 
 async function getEvent(input){
-	const event = await fromCSV();
-	console.log(events[input]);
-	return event;
+	const eventsList = await getArray();
+	//console.log(eventsList[input]);
+	return eventsList[input];
 }
 
-console.log(getArray());
-var input = 1;
-console.log(getEvent(input));
+async function main(eventsList){
+eventsList = await getArray();
+console.log(eventsList);
+//currentEvent = await getEvent(1);
+//console.log(currentEvent.getTitle);
 
 
-
-
-
-// addTitle="food 3";
-// addDescription ="this is bacon";
+// addTitle="dentist";
+// addDescription ="go and get dentist appointment";
 // addTag1=true;
 // addTag2=false;
 // addTag3=false;
 // addTag4=true;
-// addStartDate=628021600000;
-// addEndDate=628021600000;
+// addStartDate=1683897922000;
+// addEndDate=1683897922000;
+//addEvent(addTitle,addDescription,addTag1,addTag2,addTag3,addTag4,addStartDate,addEndDate);
+}
 
 
-// addEvent(addTitle,addDescription,addTag1,addTag2,addTag3,addTag4,addStartDate,addEndDate);
-
-// console.log(events)
-
-
-
-
-
-
-
-
-
-
-
-
-
+let eventsList = []
+main(eventsList);
