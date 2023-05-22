87bf3ccf9b9b8dd69818d273ca90cef987fa2160
BenD <61838571+BenDDavidson@users.noreply.github.com>
2023-05-22 16:08:43 +0100

    loading and saving

--- Homepage/HomePage.js
+++ Homepage/HomePage.js
@@ -1,3 +1,121 @@
+class Event {
+    constructor(id,title, description, tagList, startDate, endDate) {
+        this.id = id
+        this.title = title;
+        this.description = description;
+        this.tagList = tagList;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+    set changeid(newId) {this.id = newid;}
+    get getid(){return this.id;}
+
+    set changeTitle(newTitle) {this.title = newTitle;}
+    get getTitle(){return this.title;}
+
+    set changeDescription(newDescription) {this.description = newDescription;}
+    get getDescription(){return this.description;}
+
+    set changeTagList(newTaglist) {this.tagList= newTaglist;}
+    get getTagList(){return this.tagList;}
+
+    set changeStartDate(newStartDate) {this.startDate = newStartDate;}
+    get getStartDate(){return this.startDate;}
+
+    set changeEndDate(newEndDate) {this.endDate = newEndDate;}
+    get getEndDate(){return this.endDate;}
+
+}
+
+function createEvent(id,title, description, tagList, startDate, endDate) {
+
+    const eventBoardItem = document.createElement("button");
+    eventBoardItem.className = "eventBoard__Item";
+
+    eventBoardItem.setAttribute("onclick", "eventClick(this)");
+
+    const itemContent = document.createTextNode(`${title} ${description} ${tagList} ${startDate} ${endDate}`);
+
+    eventBoardItem.appendChild(itemContent);
+
+    const EventBoard = document.getElementById("AllBoard");
+    EventBoard.querySelector(".eventBoard__Item_List").appendChild(eventBoardItem);
+}
+
+
+if (typeof(Storage) !== "undefined") {
+    // Retrieve the events from local storage
+    let storedEvents = localStorage.getItem("events");
+
+    // Check if there are previously stored events
+    var events = storedEvents ? JSON.parse(storedEvents) : [];
+
+    // Function to add a new event#
+    function addEvent(addID,addTitle,addDescription,addTagsList,addStartDate,addEndDate) {
+        // Create a new event object
+        const id = addID;
+        const title = addTitle;
+        const description = addDescription;
+        const tagList = addTagsList;
+        const startDate = new Date (parseInt(addStartDate));
+        const endDate = new Date (parseInt(addEndDate));
+
+        const event = new Event(id,title,description,tagList,startDate,endDate);
+
+        // Add the event to the array
+        events.push(event);
+
+        // Save the updated array to local storage
+        localStorage.setItem("events", JSON.stringify(events));
+        console.log(events)
+    }
+
+    function deleteEvent(index) {
+        // Remove the event from the array
+        events.splice(index, 1);
+
+        // Save the updated array to local storage
+        localStorage.setItem("events", JSON.stringify(events));
+    }
+
+    // Example usage:
+    addId = 0;
+    addTitle="add this";
+    addDescription ="go and get ice";
+    addTag1=true;
+    addTag2=false;
+    addTag3=false;
+    addTag4=true;
+    addStartDate=1683897922000;
+    addEndDate=1683897922000;
+
+    //addEvent(addId,addTitle,addDescription,addTag1,addTag2,addTag3,addTag4,addStartDate,addEndDate);
+    //deleteEvent(0,events);
+    //addEvent('Test','desc',true,true,false,false,1683897922000,1683897922000);
+
+    //Retrieve the updated events from local storage
+    storedEvents = localStorage.getItem("events");
+    events = storedEvents ? JSON.parse(storedEvents) : [];
+
+
+    //currentEvent = events.find(item=> item.id === 0)
+    // Display the events
+    //console.log(events[0].title);
+    console.log(events);
+
+    //-------------------------LOAD EVENTS --------------------------------
+
+    for (let i = 0; i < events.length; i++) {
+        createEvent(events[i].id,events[i].title,events[i].description,events[i].tagList,events[i].startDate,events[i].endDate);
+    }
+
+} else {
+    console.log("Local storage is not supported.");
+}
+
+
+// -------------------------POPUP--------------------------------
+
 // Get the modal
 var modal = document.getElementById("myModal");
 
@@ -67,7 +185,7 @@ function saveTextarea() {
     return String(textarea)
 }
 
-function saveChecklist() {
+function saveTagslist() {
     var option1 = document.getElementById("option1").checked;
     var option2 = document.getElementById("option2").checked;
     var option3 = document.getElementById("option3").checked;
@@ -78,29 +196,18 @@ function saveChecklist() {
 
 //use this function to create event on page using data
 function saveAll(){
-    var eventValue = [saveTitle(), saveTextarea(),saveChecklist(),saveDate(),saveEndDate()]
+    var id = events.length;
+
+    var eventValue = [id, saveTitle(), saveTextarea(),saveTagslist(),saveDate(),saveEndDate()]
     
-    //console.log(eventValue)
-
-    // Create a new event board item with the collected data
-    const eventBoardItem = document.createElement("button");
-    eventBoardItem.className = "eventBoard__Item";
-
-    eventBoardItem.setAttribute("onclick", "eventClick(this)");
+    createEvent(id, saveTitle(), saveTextarea(),saveTagslist(),saveDate(),saveEndDate())
 
 
-    const popup = document.createElement("div");
-
-
-
-    const itemContent = document.createTextNode(`${eventValue[0]} ${eventValue[1]} ${eventValue[2]} ${eventValue[3]} ${eventValue[4]}`);
-
-    eventBoardItem.appendChild(itemContent);
-
-    const EventBoard = document.getElementById("AllBoard");
-    EventBoard.querySelector(".eventBoard__Item_List").appendChild(eventBoardItem);
+    addEvent(eventValue[0],eventValue[1],eventValue[2],eventValue[3],eventValue[4],eventValue[5])
 
     modal.style.display = "none";
+
+
 }
 
 
