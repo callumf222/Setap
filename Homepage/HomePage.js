a5492aa935617c340ce1d0bcabb77b594b2a8523
up2047715 <100426318+up2047715@users.noreply.github.com>
2023-05-23 15:25:42 +0100

    Made everything look better and run how it should

--- Homepage/HomePage.js
+++ Homepage/HomePage.js
@@ -27,6 +27,55 @@ class Event {
 
 }
 
+function convertTimestamp(timestamp) {
+    var d = new Date(timestamp), // Convert the passed timestamp to milliseconds
+        yyyy = d.getFullYear(),
+        mm = ('0' + (d.getMonth() + 1)).slice(-2),  // Months are zero based. Add leading 0.
+        dd = ('0' + d.getDate()).slice(-2),         // Add leading 0.
+        hh = d.getHours(),
+        h = hh,
+        min = ('0' + d.getMinutes()).slice(-2),     // Add leading 0.
+        ampm = 'AM',
+        time;
+
+    if (hh > 12) {
+        h = hh - 12;
+        ampm = 'PM';
+    } else if (hh === 12) {
+        h = 12;
+        ampm = 'PM';
+    } else if (hh == 0) {
+        h = 12;
+    }
+
+    // ie: 2014-03-24, 3:00 PM
+    time = yyyy + '-' + mm + '-' + dd;
+    return time;
+}
+
+function convertTagList(tagList) {
+    let result = "";
+    
+    if (tagList[0]) {
+      result += "Work ";
+    }
+    
+    if (tagList[1]) {
+      result += "Social ";
+    }
+    
+    if (tagList[2]) {
+      result += "Chores ";
+    }
+    
+    if (tagList[3]) {
+      result += "Reminders";
+    }
+    
+    return result.trim();
+  }
+  
+
 function createEventBoard(id,title, description, tagList, startDate, endDate) {
 
     const eventBoardItem = document.createElement("button");
@@ -35,7 +84,7 @@ function createEventBoard(id,title, description, tagList, startDate, endDate) {
     eventBoardItem.setAttribute("id", id);
     eventBoardItem.setAttribute("onclick", "eventClick(this)");
 
-    const itemContent = document.createTextNode(`${title} ${description} ${tagList} ${startDate} ${endDate}`);
+    const itemContent = document.createTextNode(`TItle: ${title} | Description: ${description} | Tags: ${convertTagList(tagList)} | StartDate: ${convertTimestamp(startDate)} | EndDate: ${convertTimestamp(endDate)}`);
 
     eventBoardItem.appendChild(itemContent);
 
