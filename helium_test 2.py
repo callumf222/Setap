f95f3d76a8acb0ff44269dde11a2d76a2fbda72d
up2047715 <100426318+up2047715@users.noreply.github.com>
2023-05-25 11:31:06 +0100

    Helium Shit

---
+++ helium_test 2.py
@@ -0,0 +1,23 @@
+from helium import *
+
+start_firefox()
+go_to("http://127.0.0.1:5500/Homepage/HomePage.html")
+click('+')
+write('Get Bacon', into='Event Title')
+write('Fill up the cart with eggs, bacon, bread and other food items for the week', into='Description')
+click('Chores')
+click('Reminder')
+click('Save All')
+
+click('+')
+write('Go to work', into='Event Title')
+write('Be ready to go to your job at 9pm!', into='Description')
+click('Work')
+click('Chores')
+click('Reminder')
+click('Save All')
+
+click('Work')
+click('Work')
+click('Chores')
+print('done')
